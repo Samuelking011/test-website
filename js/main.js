a62c70f0361858ae5a94d9694627fa05bdafda7c
Samuel Olayinka <olayinkasamuel44@gmail.com>
2023-10-26 16:12:12 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const { default: Swiper } = require("swiper");
+
+//initialize WoW JS
+new WOW().init();
+
+//initialize swippers
+var aboutSwiper = new Swiper(".aboutSwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        freeMode: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            // When window width is <= 599px
+            599: {
+                slidesPerView: 2,
+                spaceBetweenSlides: 50
+            },
+        }
+    }
+);
+
+var planeSwiper = new Swiper(".planeSwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+//Get The Elements
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+
+//Toggle the navbar menu on click Open/Close
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle('nav-toggle');
+});
+
+//Get the current year and date into the HTML
+document.querySelector(".year").innerHTML = new Date().getFullYear();
