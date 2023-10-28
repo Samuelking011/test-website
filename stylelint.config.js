eec82a5156eaeaaf7829672531de5c4574a1f5bd
Samuelking011 <116525081+Samuelking011@users.noreply.github.com>
2023-10-28 13:48:05 -0700

    Update stylelint.config.js

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,11 +1,20 @@
 module.exports = {
-  #"extends": "stylelint-config-standard",
-  "plugins": ["stylelint-css"],
-  "rules": {
-    "plugin/stylelint-css": true,
-    "property-no-unknown": true
+  extends: 'stylelint-config-standard',
+  plugins: ['stylelint-order'],
+  rules: {
+    'order/order': [
+      'custom-properties',
+      'dollar-variables',
+      'declarations',
+      'rules',
+      'at-rules',
+    ],
+    'order/properties-alphabetical-order': true, // Enfore alphabetical order of properties
+    'selector-pseudo-element-colon-notation': 'single', // Use single colon for pseudo-elements (::before)
+    #"plugin/stylelint-css": true,
+    #"property-no-unknown": true
     #"at-rule-no-unknown": null,
     #"css/at-rule-no-unknown": true,
     #"no-descending-specificity": null
   },
-}
+};
