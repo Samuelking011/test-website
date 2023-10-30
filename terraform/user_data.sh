3bbd9f990237ad122be6579b0a2d7f70d43f89ca
Samuel Olayinka <olayinkasamuel44@gmail.com>
2023-10-30 14:58:44 -0700

    modified

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -7,3 +7,6 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 sudo yum install npm -y
+
+sudo docker pull samuelking01/website:latest
+sudo docker run -d -p 80:80 samuelking01/website:latest
